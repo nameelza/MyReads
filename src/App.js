ba077fbda75b4803fc1b40724f84f1e4b6fb73cb
nameelza <82921161+nameelza@users.noreply.github.com>
2022-02-23 15:13:37 -0800

    componentDidMount inCategories

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-  };
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       console.log(books);
+      let inCategories = {};
+      books.map((book) => {
+        if (book.shelf !== "none") {
+          if (!inCategories[book.shelf]) {
+            inCategories[book.shelf] = [];
+          }
+          inCategories[book.shelf].push(book);
+          console.log(inCategories)
+        }
+      })
     });
   }
 
