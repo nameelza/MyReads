444c3dc5c5c146b7532a132a36d519d7662337cb
nameelza <82921161+nameelza@users.noreply.github.com>
2022-02-24 13:57:15 -0800

    AllBooks

--- src/Shelf.js
+++ src/Shelf.js
@@ -2,38 +2,40 @@ import React, { Component } from "react";
 
 class Shelf extends Component {
   render() {
-    console.log("Shelffff", this.props.shelfBooks);
     return (
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                />
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
+          {this.props.shelfBooks.map((book) => (
+            <li key={book.title}>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${book.imageLinks.smallThumbnail})`,
+                    }}
+                  />
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="move" disabled>
+                        Move to...
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
                 </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors.join(", ")}</div>
               </div>
-              <div className="book-title">To Kill a Mockingbird</div>
-              <div className="book-authors">Harper Lee</div>
-            </div>
-          </li>
+            </li>
+          ))}
         </ol>
       </div>
     );
