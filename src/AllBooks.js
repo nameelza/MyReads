fe1cb10659b7e854d641fccc12a67a1ffdfafa19
nameelza <82921161+nameelza@users.noreply.github.com>
2022-02-24 13:44:35 -0800

    AllBooks

--- src/AllBooks.js
+++ src/AllBooks.js
@@ -2,32 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Shelf from "./Shelf";
 
-function getName(str) {
-  switch (str) {
-    case "dd":
-      return "Currently Reading";
-    case "ddd":
-      return "Want to read";
-    case "read":
-      return "Read";
-    default:
-      return null;
-  }
-}
+
 
 function AllBooks({books}) {
-  console.log("allBooks Component", books);
+    function getName(str) {
+        switch (str) {
+          case "currentlyReading":
+            return "Currently Reading";
+          case "wantToRead":
+            return "Want to Read";
+          case "read":
+            return "Read";
+          default:
+            return null;
+        }
+      }
   return (
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
+    <>
       <div className="list-books-content">
         <div>
           {Object.keys(books).map((shelf) => (
-            <div className="bookshelf">
+            <div className="bookshelf" key={shelf}>
               <h2 className="bookshelf-title">{getName(shelf)}</h2>
-              <Shelf shelfBooks={books} />
+              <Shelf shelfBooks={books[shelf]} />
             </div>
           ))}
         </div>
@@ -37,7 +34,7 @@ function AllBooks({books}) {
           <button>Add a book</button>
         </Link>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/App.js
+++ src/App.js
@@ -30,39 +30,44 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={<AllBooks books={this.state.books}/>}
-          />
-          <Route
-            path="/search"
-            element={(
-              <div className="search-books">
-                <div className="search-books-bar">
-                  <Link to="/">
-                    <button className="close-search">Close</button>
-                  </Link>
-                  <div className="search-books-input-wrapper">
-                    {/*
-                      NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                      You can find these search terms here:
-                      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<AllBooks books={this.state.books}/>}
+            />
+            <Route
+              path="/search"
+              element={(
+                <div className="search-books">
+                  <div className="search-books-bar">
+                    <Link to="/">
+                      <button className="close-search">Close</button>
+                    </Link>
+                    <div className="search-books-input-wrapper">
+                      {/*
+                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                        You can find these search terms here:
+                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                      you don't find a specific author or title. Every search is limited by search terms.
-                    */}
-                    <input type="text" placeholder="Search by title or author" />
+                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                        you don't find a specific author or title. Every search is limited by search terms.
+                      */}
+                      <input type="text" placeholder="Search by title or author" />
+                    </div>
+                  </div>
+                  <div className="search-books-results">
+                    <ol className="books-grid" />
                   </div>
                 </div>
-                <div className="search-books-results">
-                  <ol className="books-grid" />
-                </div>
-              </div>
-            )}
-          />
-        </Routes>
+              )}
+            />
+          </Routes>
+        </div>
       </div>
     );
   }
