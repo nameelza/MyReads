03446eebd819d35f134c81b65ab35d8369952e6b
nameelza <82921161+nameelza@users.noreply.github.com>
2022-02-24 12:55:53 -0800

    Shelf

--- src/AllBooks.js
+++ src/AllBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Shelf from "./Shelf"
 
 class AllBooks extends Component {
   render() {
@@ -10,43 +11,18 @@ class AllBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            {/* loop over state */}
             <div className="bookshelf">
-              <h2 className="bookshelf-title">Heading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div
-                          className="book-cover"
-                          style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage:
-                              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                          }}
-                        />
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>
-                              Move to...
-                            </option>
-                            <option value="currentlyReading">
-                              Currently Reading
-                            </option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                </ol>
-              </div>
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <Shelf/>
             </div>
-            {/* close tag */}
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <Shelf/>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <Shelf/>
+            </div>
           </div>
         </div>
         <div className="open-search">
