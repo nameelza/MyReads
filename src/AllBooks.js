23311ce02d1f83fa717d394ae5f1d8cd029ba891
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-03 12:54:58 -0800

    all books component

--- src/AllBooks.js
+++ src/AllBooks.js
@@ -1,42 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
 import Shelf from "./Shelf";
 
+function getName(str) {
+  switch (str) {
+    case "currentlyReading":
+      return "Currently Reading";
+    case "wantToRead":
+      return "Want to Read";
+    case "read":
+      return "Read";
+    default:
+      return null;
+  }
+}
 
-function AllBooks({books}) {
-    function getName(str) {
-        switch (str) {
-          case "currentlyReading":
-            return "Currently Reading";
-          case "wantToRead":
-            return "Want to Read";
-          case "read":
-            return "Read";
-          default:
-            return null;
+class AllBooks extends Component {
+  state = {
+    books: {}
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      let inCategories = {};
+      books.forEach((book) => {
+        if (book.shelf !== "none") {
+          if (!inCategories[book.shelf]) {
+            inCategories[book.shelf] = [];
+          }
+          inCategories[book.shelf].push(book);
         }
-      }
-  return (
-    <>
-      <div className="list-books-content">
-        <div>
-          {Object.keys(books).map((shelf) => (
-            <div className="bookshelf" key={shelf}>
-              <h2 className="bookshelf-title">{getName(shelf)}</h2>
-              <div className="bookshelf-books">
-                <Shelf shelfBooks={books[shelf]} />
+      })
+      this.setState({books: inCategories});
+    });
+  }
+  render() {
+    return (
+      <>
+        <div className="list-books-content">
+          <div>
+            {Object.keys(this.props.books).map((shelf) => (
+              <div className="bookshelf" key={shelf}>
+                <h2 className="bookshelf-title">{getName(shelf)}</h2>
+                <div className="bookshelf-books">
+                  <Shelf shelfBooks={this.props.books[shelf]} />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="open-search">
-        <Link to="/search">
-          <button>Add a book</button>
-        </Link>
-      </div>
-    </>
-  );
+        <div className="open-search">
+          <Link to="/search">
+            <button>Add a book</button>
+          </Link>
+        </div>
+      </>
+    );
+  }
+  
 }
 
 export default AllBooks;
